2d0e5ea5189d3ff448e9f3ce29d03d29018c573b
dungkt1 <dungkt1@vnpay.vn>
2024-10-12 22:43:20 +0700

    [ktdung] reformat code

--- src/main/java/com/example/kvtag/controllers/PriceProfileDiagramController.java
+++ src/main/java/com/example/kvtag/controllers/PriceProfileDiagramController.java
@@ -24,7 +24,7 @@ public class PriceProfileDiagramController {
 
     @GetMapping
     public ResponseEntity<ApiResponse<PriceProfileDiagramDto>> getAll(@RequestParam(defaultValue = "0") int page,
-                                                                   @RequestParam(defaultValue = "10") int size) {
+                                                                      @RequestParam(defaultValue = "10") int size) {
         Page<PriceProfileDiagramDto> diagrams = priceProfileDiagramService.getAll(
                 PageRequest.of(page, size)
         );
--- src/main/java/com/example/kvtag/services/impl/PriceProfileDiagramService.java
+++ src/main/java/com/example/kvtag/services/impl/PriceProfileDiagramService.java
@@ -10,8 +10,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -83,11 +81,4 @@ public class PriceProfileDiagramService implements IPriceProfileDiagramService {
         return diagram;
     }
 
-    private List<PriceProfileDiagramDto> convertToDtoList(Iterable<PriceProfileDiagram> diagrams) {
-        List<PriceProfileDiagramDto> dtoList = new ArrayList<>();
-        for (PriceProfileDiagram diagram : diagrams) {
-            dtoList.add(convertToDto(diagram));
-        }
-        return dtoList;
-    }
 }
